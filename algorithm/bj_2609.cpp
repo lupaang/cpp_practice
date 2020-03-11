e390deaf14f4a19fe69bd06e9b1bbcaf7abbd097
uhyunpark <ericpark@bluehole.net>
2020-03-11 14:14:26 +0900

    20200311 commit

--- algorithm/bj_2609.cpp
+++ algorithm/bj_2609.cpp
@@ -10,14 +10,83 @@
 첫째 줄에는 입력으로 주어진 두 수의 최대공약수를,둘째 줄에는 입력으로 주어진 두 수의 최소 공배수를 출력한다.
 */
 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int x, y;
+//     // cin >> x >> y;
+    
+//     x = 72;
+//     y = 27;
+
+//     if (x == y)
+//     {
+//         cout << x << "\n";
+//         cout << x << "\n";
+//     }
+    
+//     else
+//     {
+//         int bigger = x > y ? x : y;
+//         int smaller = x < y ? x : y;
+
+//         if (bigger%smaller == 0)
+//         {
+//             cout << smaller << "\n";
+//             cout << bigger << "\n";
+//         }
+
+//         else
+//         {
+//             for (int i = 2; i <= smaller; i++)
+//             {
+//                 if (smaller % i == 0 && bigger % i == 0)
+//                 {
+//                     cout << i << "\n";
+//                     break;
+//                 }
+//             }
+
+//             for (int i = 1; i <= bigger; i++)
+//             {
+//                 if ((smaller * i) % bigger == 0)
+//                 {
+//                     cout << smaller * i << "\n";
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+
 #include <iostream>
 using namespace std;
 
+int gcd(int a, int b)
+{
+    int n;
+    while(b != 0)
+    {
+        n = a % b;
+        a = b;
+        b = n;
+    }
+
+    return a;
+}
+
 int main()
 {
-    int x, y;
-    cin >> x >> y;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a, b;
 
-    int max = 0;
-    int min = 10000;
+    cin >> a >> b;
+
+    cout << gcd(a, b) << "\n" << (a * b) / gcd(a,b);
 }
